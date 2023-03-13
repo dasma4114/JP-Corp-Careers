01d12c5dbded62f7a520b1b3dfc541614fc003d0
JP <67994969+dasma4114@users.noreply.github.com>
2023-03-13 20:34:12 +0000

    Add flavor text, centered heading

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JP-Corp-Careers
+
+Banner photo was by Ant Rozetsky on Unspash
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h2>JP Corp Careers</h2>"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
