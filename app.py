48c35681eae71ba1b0cfa3e280749e576f308732
JP <67994969+dasma4114@users.noreply.github.com>
2023-03-17 21:17:05 +0000

    Add footer and contact us button

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ JOBS = [
     {
         'id': 1,
         'title': 'data analyst',
-        'location': 'San Francisco, USA'
-        'salary': 105,000
+        'location': 'San Francisco, USA',
+        'salary': "$105,000"
     },
     {
         'id': 2,
         'title': 'data scientist',
-        'location': 'San Francisco, USA'
-        'salary': 135,000
+        'location': 'New York, USA',
+        'salary': "$135,000"
     },
     {
         'id': 3,
         'title': 'Frontend Dev',
-        'location': 'San Francisco, USA'
-        'salary': 125,000
+        'location': 'San Francisco, USA',
+        'salary': "$125,000"
     },
     {
         'id': 4,
         'title': 'Backend Developer',
-        'location': 'Remote'
-        'salary': 125,000
+        'location': 'Remote',
+        'salary': "$125,000"
     },
 ]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
